0c0c9e7cd4ab6345773322a9b3cddf4fa015744a
= <yordanrivera@yahoo.com>
2021-06-30 10:12:28 +0200

    modified css

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    mode: 'development',
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use:['style-loader','css-loader', {loader: 'postcss-loader', options:{postcssOptions: {plugins: postCSSPlugins}}}]
+            }
+        ]
+    }
+}
