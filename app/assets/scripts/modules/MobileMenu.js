e2517ddabe2f115243a65086adc68259783d683c
= <yordanrivera@yahoo.com>
2021-07-08 18:33:00 +0200

    Mobile header added drop down menu

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,6 +1,9 @@
 class MobileMenu {
     constructor() {
         this.menuIcon = document.querySelector(".site-header__menu-icon")
+        this.menuContent = document.querySelector(".site-header__menu-content")
+        this.siteHeader = document.querySelector(".site-header")
+        this.events()
     }
 
     events() {
@@ -8,7 +11,9 @@ class MobileMenu {
     }
 
     toggleTheMenu() {
-        console.log("Hoorey - the icon was clicked")
+        this.menuContent.classList.toggle("site-header__menu-content--is-visible")
+        this.siteHeader.classList.toggle("site-header--is-expanded")
+        this.menuIcon.classList.toggle("site-header__menu-icon--close-x")
     }
 }
 
